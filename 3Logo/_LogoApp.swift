de8ee476c24bd847d7eb17caf44a4127141ea34d
Art <artem_chernenko@ro.ru>
2022-03-06 13:36:41 +0300

    Initial Commit

---
+++ 3Logo/_LogoApp.swift
@@ -0,0 +1,17 @@
+//
+//  _LogoApp.swift
+//  3Logo
+//
+//  Created by Артем Черненко on 06.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct _LogoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
