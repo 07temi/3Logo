67e4d324c4502bd55019600d14818e0b9e553e02
Art <artem_chernenko@ro.ru>
2022-03-07 10:41:41 +0300

    done

--- StartLogo/StartLogo/MainView.swift
+++ StartLogo/StartLogo/MainView.swift
@@ -9,52 +9,61 @@ import SwiftUI
 
 struct MainView: View {
     @State private var showAward = false
+    @State private var logoAnimation = false
+    
+    @State private var angle = 0.0
     
     var body: some View {
         VStack {
-            Button(action: { showAward.toggle() }) {
+            Button(action: buttonAction) {
                 HStack {
                     Text(showAward ? "Hide Award" : "Show Award")
                     Spacer()
                     Image(systemName: "chevron.up.square")
                         .scaleEffect(showAward ? 2 : 1)
                         .rotationEffect(.degrees(showAward ? 0 : 180))
-                        //.animation(.easeIn(duration: 2.0))
                 }
             }
             Spacer()
-            MyLogo(size: 200)
-                .rotationEffect(.degrees(showAward ? -15 : 15))
-                .animation(.spring().repeatCount(10, autoreverses: true))
-            //if showAward {
-                //GradientRectangles()
-                 //   .frame(width: 250, height: 250)
-                    //.transition(.transition)
-           // }
-
-           // Spacer()
-//        }
-//        .font(.headline)
-//        .padding()
-//    }
-    
-//    private func buttonAction() {
-//        withAnimation {
-//            showAward.toggle()
-//        }
+            if showAward {
+                MyLogo()
+                    .frame(width: 250, height: 250)
+                    .transition(.transition)
+                    .rotation3DEffect(.degrees(angle), axis: (x: 0, y: 1, z: 0))
+                    .animation(.easeOut.speed(0.3).repeatCount(1, autoreverses: false))
+                    .onTapGesture {
+                        if !logoAnimation {
+                            angle = 720
+                            logoAnimation.toggle()
+                        } else {
+                            angle = 0
+                            logoAnimation.toggle()
+                        }
+                    }
+            }
+            Spacer()
+            
+        }
+        .font(.headline)
+        .padding()
+    }
+    private func buttonAction() {
+        withAnimation {
+            showAward.toggle()
+        }
     }
 }
+
+extension AnyTransition {
+    static var transition: AnyTransition {
+        let insertion = AnyTransition.move(edge: .leading)
+        let removal = AnyTransition.scale
+            .combined(with: .opacity)
+        
+        return .asymmetric(insertion: insertion, removal: removal)
+    }
 }
 
-//extension AnyTransition {
-//    static var transition: AnyTransition {
-//        let insertion = AnyTransition.move(edge: .leading)
-//        let removal = AnyTransition.scale
-//            .combined(with: .opacity)
-//
-//        return .asymmetric(insertion: insertion, removal: removal)
-//    }
-//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- StartLogo/StartLogo/MyLogo.swift
+++ StartLogo/StartLogo/MyLogo.swift
@@ -8,27 +8,24 @@
 import SwiftUI
 
 struct MyLogo: View {
-    let size: Int
     var body: some View {
         GeometryReader { geometry in
-//            Можно было уйти от параметров фрейма, но тогда много возни с приведением типов
-//            получалось
+//да тут немного наворотил, зато сохраняются пропорции при любом размере фрейма (почти)))
             let width = geometry.size.width
             let height = geometry.size.height
-            
+            //черный контур
             Path { path in
-                path.move(to: CGPoint(x: width / 4, y: 0)) //верхний край
-                path.addLine(to: CGPoint(x: width / 2.1, y: height / 5.5)) //макушка
-                path.addLine(to: CGPoint(x: width / 1.9, y: height / 5.5)) //правое ухо
-                path.addLine(to: CGPoint(x: width / 1.3, y: 0)) //верхний край
-                path.addLine(to: CGPoint(x: width / 1.2, y: height / 3)) //правое ухо низ
-                path.addLine(to: CGPoint(x: width, y: height / 1.7)) //правй край
-                path.addLine(to: CGPoint(x: width / 1.66, y: height)) //подбородок
-                path.addLine(to: CGPoint(x: width / 2.5, y: height)) //подбородок
-                path.addLine(to: CGPoint(x: 0, y: height / 1.7)) //левый край
-                path.addLine(to: CGPoint(x: width / 6, y: height / 3)) //левое ухо низ
+                path.move(to: CGPoint(x: width / 4, y: 0))
+                path.addLine(to: CGPoint(x: width / 2.1, y: height / 5.5))
+                path.addLine(to: CGPoint(x: width / 1.9, y: height / 5.5))
+                path.addLine(to: CGPoint(x: width / 1.3, y: 0))
+                path.addLine(to: CGPoint(x: width / 1.2, y: height / 3))
+                path.addLine(to: CGPoint(x: width, y: height / 1.7))
+                path.addLine(to: CGPoint(x: width / 1.66, y: height))
+                path.addLine(to: CGPoint(x: width / 2.5, y: height))
+                path.addLine(to: CGPoint(x: 0, y: height / 1.7))
+                path.addLine(to: CGPoint(x: width / 6, y: height / 3))
             }
-            //БЕЛАЯ ЧАСТЬ
             //левое ухо
             Path { path in
                 path.move(to: CGPoint(x: width / 3.75, y: height / 8.55))
@@ -44,41 +41,38 @@ struct MyLogo: View {
             }
             .fill(.white)
 
-        //моська
+            //моська
             Path { path in
                 path.move(to: CGPoint(x: width / 3.15, y: height / 2.5))
                 path.addLine(to: CGPoint(x: width / 2.14, y: height / 1.81))
                 path.addLine(to: CGPoint(x: width / 2, y: height / 1.62))
                 path.addLine(to: CGPoint(x: width / 1.87, y: height / 1.81))
                 path.addLine(to: CGPoint(x: width / 1.46, y: height / 2.5))
-            //
                 path.addLine(to: CGPoint(x: width / 1.17, y: height / 1.93))
                 path.addLine(to: CGPoint(x: width / 1.13, y: height / 1.71))
-                path.addLine(to: CGPoint(x: width / 1.71, y: width / 1.09))
+                path.addLine(to: CGPoint(x: width / 1.71, y: height / 1.09))
 
                 path.addLine(to: CGPoint(x: width / 2, y: height / 1.05))
 
-                path.addLine(to: CGPoint(x: width / 2.4, y: width / 1.09))
+                path.addLine(to: CGPoint(x: width / 2.4, y: height / 1.09))
                 path.addLine(to: CGPoint(x: width / 8.57, y: height / 1.71))
                 path.addLine(to: CGPoint(x: width / 6.66, y: height / 1.93))
             }
             .fill(.white)
-//
             
-            //скулы
-//                //слевая
+            // скула слева
             Path { path in
                 path.move(to: CGPoint(x: width / 4.6, y: height / 1.42))
                 path.addLine(to: CGPoint(x: width / 3.75, y: height / 1.57))
                 path.addLine(to: CGPoint(x: width / 3.33, y: height / 1.25))
             }
-//                //справая
+            //скула справа
             Path { path in
                 path.move(to: CGPoint(x: width / 1.27, y: height / 1.42))
                 path.addLine(to: CGPoint(x: width / 1.36, y: height / 1.57))
                 path.addLine(to: CGPoint(x: width / 1.42, y: height / 1.25))
             }
-//            //нос
+            //нос
             Path { path in
                 path.move(to: CGPoint(x: width / 2.14, y: height))
                 path.addLine(to: CGPoint(x: width / 2.14, y: height / 1.09))
@@ -91,15 +85,15 @@ struct MyLogo: View {
 
                 path.addLine(to: CGPoint(x: width / 1.87, y: height))
             }
-//            //глаз
-//                //слева
+
+            //глаз слева
             Path { path in
                 path.move(to: CGPoint(x: width / 2.4, y: height / 1.62))
                 path.addLine(to: CGPoint(x: width / 3, y: height / 1.66))
                 path.addLine(to: CGPoint(x: width / 3.33, y: height / 1.87))
                 path.addLine(to: CGPoint(x: width / 2.6, y: height / 1.81))
             }
-//справа
+            //глаз справа
             Path { path in
                 path.move(to: CGPoint(x: width / 1.71, y: height / 1.62))
                 path.addLine(to: CGPoint(x: width / 1.5, y: height / 1.66))
@@ -107,12 +101,12 @@ struct MyLogo: View {
                 path.addLine(to: CGPoint(x: width / 1.62, y: height / 1.81))
             }
         }
-        .frame(width: CGFloat(size), height: CGFloat(size))
     }
 }
 
 struct MyLogo_Previews: PreviewProvider {
     static var previews: some View {
-        MyLogo(size: 200)
+        MyLogo()
+            .frame(width: 200, height: 200)
     }
 }
